465283ab76c38393e2843d569a79b89df8e6d55d
SamuelSalcedo <salcedolucerosmauel2@gmail.com>
2025-09-07 00:20:25 -0600

    agregando ventana vacia

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # ejercicios-utilidades
 Ejercicios para mejorar en python ademas de agregar una interface para mejor experiencia de usuario
 
+Aun en Desarrollo
+
+# Mejora de apariencia 
+Use la apariencia de [Forest-ttk-theme](https://github.com/rdbende/Forest-ttk-theme) de rdbende 
+
+
+## Deployment
+
+Para correr este proyecto ingresa el siguiente comando
+
+```bash
+  python main.py
+```
+
+## 🔗 Links
+
+
+## 🛠 Lenguajes y herramientas
+![Lenguaje](https://img.shields.io/badge/Lenguaje-Python-blue)
+
+
+## Other Common Github Profile Sections
+👩‍💻 Actualmente trabajando en proyectos para mi portafolio
+
+🧠 Actualmente estoy interesado en: python, JavaScript, React
+
+📫 Contactame: [Mi linkedin](www.linkedin.com/in/samuel-salcedo-lucero-8a26041b8)
+
+## Licencias
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
--- main.py
+++ main.py
@@ -1,1 +1,55 @@
+import tkinter as tk
 
+from tkinter import ttk, messagebox
+
+#inicilaizar la vista general que tendra el proyecto
+root = tk.Tk()
+root.title("Seguimiento de Gastos!")
+
+#agregar estilo
+root.tk.call('source', 'forest-dark.tcl')
+ttk.Style().theme_use('forest-dark')
+root.option_add("*tearOff", False) # This is always a good idea
+
+#tamanio de la ventana
+root.geometry("950x600")
+
+
+#Hacer la app responsiva
+root.columnconfigure(index=0, weight=1)
+root.columnconfigure(index=1, weight=1)
+root.columnconfigure(index=2, weight=1)
+root.rowconfigure(index=0, weight=1)
+root.rowconfigure(index=1, weight=1)
+root.rowconfigure(index=2, weight=1)
+
+#primer utilidad 
+#el notebook delimita el contenido
+# Notebook
+notebook = ttk.Notebook(root)
+
+"""
+
+# CONTENIDO DEL TAB #1
+
+"""
+
+tab_1 = ttk.Frame(notebook)
+notebook.add(tab_1, text="Pagina 1")
+
+entry = ttk.Entry(tab_1)
+entry.insert(0,"")
+entry.grid(row=0, column=0, padx=5, pady=(0, 10), sticky="ew")
+
+notebook.pack(expand=True, fill="both", padx=25, pady=25)
+
+
+#Hacer que aparezca en el centro
+root.update()
+root.minsize(root.winfo_width(), root.winfo_height())
+x_cordinate = int((root.winfo_screenwidth()/4) - (root.winfo_width()/3))
+y_cordinate = int((root.winfo_screenheight()/3) - (root.winfo_height()/4))
+root.geometry("+{}+{}".format(x_cordinate, y_cordinate))
+
+
+root.mainloop()
